e5c5c2b9d503a1f43e95942e774d06ab1da32f99
jinkyoungun <jinkyoungun@naver.com>
2026-09-08 11:06:33 +0900

    Apply approved motion icon for iPhone home screen

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'morning-calisthenics-v1';
-const CORE = ['./', './index.html', './manifest.webmanifest', './apple-touch-icon.png', './icon.svg', './icon-192.png', './icon-512.png'];
+const CACHE = 'morning-calisthenics-v2-motion';
+const CORE = ['./', './index.html', './manifest.webmanifest', './motion-icon-180.png', './motion-icon-192.png', './motion-icon-512.png'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting()));
